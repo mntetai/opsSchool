361e40bbe120ae5ef97959d8c9a35b0863466ff3
mntetai <mntetai@gmail.com>
2022-08-17 16:56:46 +0300

    added passwardless login

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+#client side ssh
+sudo su
+echo '   PasswordAuthentication yes' >> /etc/ssh/ssh_config
+echo '   StrictHostKeyChecking no' >> /etc/ssh/ssh_config
+sudo passwd -d vagrant
+sudo service sshd restart
--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+#server side - sshd
+sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication no/' /etc/ssh/sshd_config
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords yes/' /etc/ssh/sshd_config
+sudo passwd -d vagrant
+sudo service sshd restart
